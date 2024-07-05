bf071ac284fe9c46d966db4535bb9c67e33e73c8
Youssefwael397 <youssefwael397@gmail.com>
2024-07-05 19:51:05 +0300

    styling show user menu

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './tabs.css';
 import Layout from './Components/Layout';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
